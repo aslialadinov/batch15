883a0f29d45e3fa9e728ef2fe11d7e1468515886
aslialadinov <asaladinov@gmail.com>
2023-01-12 12:02:34 -0500

    v1

--- src/OperatorsDemo.java
+++ src/OperatorsDemo.java
@@ -2,10 +2,10 @@ public class OperatorsDemo {
     public static void main(String[] args) {
         int number1=10;
         int number2=20;
-        System.out.println(number1+number2);
-        System.out.println(number1-number2);
-        System.out.println(number1*number2);
-        System.out.println(number1/number2);
+        System.out.println(number1+number2); //30
+        System.out.println(number1-number2);// -10
+        System.out.println(number1*number2); //200
+        System.out.println(number1/number2); //0
 
     }
 }
