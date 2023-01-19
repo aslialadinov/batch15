d99a0e16fb50f0656b9b22ea60344989dd26e1bd
aslialadinov <asaladinov@gmail.com>
2023-01-19 00:34:44 -0500

    HW Project update

---
+++ src/HomeWorkDayProject/Task10.java
@@ -0,0 +1,25 @@
+package HomeWorkDayProject;
+
+public class Task10 {
+    public static void main(String[] args) {
+        // 10)Write a java program to find the second largest number in the array?
+        int temporary =0;
+        int size =0;
+        int []num={20,40,30,80,70,90,74,64,};
+
+        size = num.length;
+
+        for (int i = 0; true; i++) {
+            for (int j = i+1; i < size; j++) {
+                if(num[i]>num[j]){
+                    temporary = num[i];
+                    num[i] = num[j];
+                    num[j] = temporary;
+
+                }
+                System.out.println("Second largest number in this Array is "+num[size-2]);
+            }
+        }
+
+    }
+}
--- src/HomeWorkDayProject/Task7.java
+++ src/HomeWorkDayProject/Task7.java
@@ -1,8 +1,23 @@
 package HomeWorkDayProject;
 
+import java.util.Scanner;
+
 public class Task7 {
     public static void main(String[] args) {
         // 7)Write a java program to check whether a given number is prime or not?
 
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter a number to check if it is prime number");
+        int number = input.nextInt();
+        int isPrime=0;
+       // if(isPrime(number)) {
+            System.out.println(number+ " is prime number");
+        //} else {
+            System.out.println(number+" is not prime number");
+        }
+
+
+
+
     }
-}
