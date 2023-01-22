0a69bbcd54ade7fc2592f216ecce137a865b3d80
aslialadinov <asaladinov@gmail.com>
2023-01-22 15:03:51 -0500

    Class 11

---
+++ src/class12/MakeUpTester.java
@@ -0,0 +1,13 @@
+package class12;
+
+public class MakeUpTester {
+    public static void main(String[] args) {
+
+        MakeUp loreal=new MakeUp();
+        loreal.make="Loreal";
+        loreal.brand="Glossy";
+        loreal.color="Bright";
+        loreal.Price=20;
+        loreal.cost();
+    }
+}
