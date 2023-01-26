ab243865c9eed999fdd5767e3a2dca8852a197fa
aslialadinov <asaladinov@gmail.com>
2023-01-25 22:11:10 -0500

    Class 14

--- src/class12/StringDemo3.java
+++ src/class12/StringDemo3.java
@@ -2,6 +2,7 @@ package class12;
 
 public class StringDemo3 {
     public static void main(String[] args) {
+
         String firstName="Akhter";
         String lastName="Lava";
         String fullName=firstName+" "+lastName; // most widely used
---
+++ src/class14/Task1.java
@@ -0,0 +1,13 @@
+package class14;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        //1) Create a String that will hold a sentence. Write a program to get a new String without any spaces.
+        String str=" Batch 15 is Great";
+        String newstr=str.replaceAll(" ","");
+        System.out.println(newstr);
+
+
+    }
+}
