8cdd436a65187d63ceb1b7d3cb7eca0e7122cd10
aslialadinov <asaladinov@gmail.com>
2023-02-15 22:33:13 -0500

    Class 26

--- src/class25/MovAbleTester.java
+++ src/class25/MovAbleTester.java
@@ -6,6 +6,7 @@ public class MovAbleTester {
 
         for(MovAble m:washAble){
             m.move();
+
         }
     }
 
---
+++ src/class26/Dog.java
@@ -0,0 +1,8 @@
+package class26;
+
+public class Dog {
+    private String name;
+    private String color;
+    private String breed;
+
+}
